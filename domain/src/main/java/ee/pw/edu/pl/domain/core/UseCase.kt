1a47de01a53b29371d82049290d0a46585b5bae7
Michal Ankiersztajn <michalankiersztajn@gmail.com>
2023-05-08 19:59:50 +0200

    Remove UseCase interface and add coroutines

--- domain/src/main/java/ee/pw/edu/pl/domain/core/UseCase.kt
+++
@@ -1,5 +0,0 @@
-package ee.pw.edu.pl.domain.core
-
-interface UseCase<in Param, out Result> {
-    suspend operator fun invoke(param: Param): Result
-}
--- domain/src/main/java/ee/pw/edu/pl/domain/usecase/test/GetTestUseCase.kt
+++ domain/src/main/java/ee/pw/edu/pl/domain/usecase/test/GetTestUseCase.kt
@@ -1,10 +1,9 @@
 package ee.pw.edu.pl.domain.usecase.test
 
-import ee.pw.edu.pl.domain.core.UseCase
 import javax.inject.Inject
 
 class GetTestUseCase @Inject constructor(
     private val testRepository: TestRepository,
-) : UseCase<Unit, Test> {
-    override suspend fun invoke(param: Unit) = testRepository.getTest()
+) {
+    suspend operator fun invoke() = testRepository.getTest()
 }
