73ab91d12d3e7c4c7f1e6609d9011978b577a517
Michal Ankiersztajn <michalankiersztajn@gmail.com>
2023-05-15 19:54:09 +0200

    Add UseCaseResult with ResultErrorType and message mapping

---
+++ app/src/main/java/ee/pw/edu/pl/doborpartnera/core/result/ResultErrorTypeMessage.kt
@@ -0,0 +1,13 @@
+package ee.pw.edu.pl.doborpartnera.core.result
+
+import androidx.annotation.StringRes
+import ee.pw.edu.pl.doborpartnera.R
+import ee.pw.edu.pl.domain.core.result.ResultErrorType
+
+@StringRes
+fun ResultErrorType.getMessage() = when (this) {
+    ResultErrorType.INVALID_CREDENTIALS -> R.string.result_err_invalid_credentials
+    ResultErrorType.EMAIL_TAKEN -> R.string.result_err_email_taken
+    ResultErrorType.NETWORK -> R.string.result_err_network
+    ResultErrorType.UNKNOWN -> R.string.result_err_unknown
+}
---
+++ domain/src/main/java/ee/pw/edu/pl/domain/core/result/ResultErrorType.kt
@@ -0,0 +1,13 @@
+package ee.pw.edu.pl.domain.core.result
+
+enum class ResultErrorType {
+    //    Login
+    INVALID_CREDENTIALS,
+
+    //    Register
+    EMAIL_TAKEN,
+
+    //    Common
+    NETWORK,
+    UNKNOWN,
+}
