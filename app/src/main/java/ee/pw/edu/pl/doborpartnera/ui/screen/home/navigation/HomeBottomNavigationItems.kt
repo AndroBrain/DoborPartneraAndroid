d626751ab9e291ed1e876a4ad286639440b3f20b
Michal Ankiersztajn <michalankiersztajn@gmail.com>
2023-05-17 08:42:43 +0200

    Update HomeBottomNavigationItems looks

--- app/src/main/java/ee/pw/edu/pl/doborpartnera/ui/screen/home/navigation/HomeBottomNavigationItems.kt
+++ app/src/main/java/ee/pw/edu/pl/doborpartnera/ui/screen/home/navigation/HomeBottomNavigationItems.kt
@@ -2,8 +2,9 @@ package ee.pw.edu.pl.doborpartnera.ui.screen.home.navigation
 
 import androidx.annotation.StringRes
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Chat
-import androidx.compose.material.icons.filled.HeartBroken
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.ui.graphics.vector.ImageVector
 import ee.pw.edu.pl.doborpartnera.R
 import ee.pw.edu.pl.doborpartnera.ui.screen.chats.CHATS_ROUTE
@@ -16,6 +17,10 @@ enum class HomeBottomNavigationItems(
     val route: String,
 ) {
     CHATS(title = R.string.chats_title, icon = Icons.Default.Chat, route = CHATS_ROUTE),
-    MATCH(title = R.string.match_title, icon = Icons.Default.HeartBroken, route = MATCH_ROUTE),
-    PROFILE(title = R.string.profile_title, icon = Icons.Default.Chat, route = PROFILE_ROUTE),
+    MATCH(title = R.string.match_title, icon = Icons.Default.Favorite, route = MATCH_ROUTE),
+    PROFILE(
+        title = R.string.profile_title,
+        icon = Icons.Default.AccountCircle,
+        route = PROFILE_ROUTE,
+    ),
 }
