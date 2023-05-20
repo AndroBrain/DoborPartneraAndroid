f816256f78ff5e66a1cad4fc4e6451d0e5d84549
Michal Ankiersztajn <michalankiersztajn@gmail.com>
2023-05-20 08:43:18 +0200

    Add app icon

--- app/src/main/java/ee/pw/edu/pl/doborpartnera/ui/screen/auth/AuthScreen.kt
+++ app/src/main/java/ee/pw/edu/pl/doborpartnera/ui/screen/auth/AuthScreen.kt
@@ -1,15 +1,13 @@
 package ee.pw.edu.pl.doborpartnera.ui.screen.auth
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.HeartBroken
 import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
@@ -17,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import ee.pw.edu.pl.doborpartnera.R
@@ -42,11 +41,10 @@ fun AuthScreen(
                     .fillMaxWidth(),
                 horizontalAlignment = Alignment.CenterHorizontally,
             ) {
-                Icon(
+                Image(
                     modifier = Modifier.size(App.dimens.logo_size),
-                    imageVector = Icons.Default.HeartBroken,
+                    painter = painterResource(id = R.drawable.ic_logo),
                     contentDescription = null,
-                    tint = MaterialTheme.colorScheme.primary,
                 )
                 Text(
                     modifier = Modifier.padding(vertical = App.dimens.views_spacing_small),
