2dccf32c2e08dafba79418e105f0c2bcdfe86bc5
Michal Ankiersztajn <michalankiersztajn@gmail.com>
2023-11-08 12:29:24 +0100

    Use Channels to upload multiple images faster

--- data/src/main/java/ee/pw/edu/pl/data/repository/ProfileRepositoryImpl.kt
+++ data/src/main/java/ee/pw/edu/pl/data/repository/ProfileRepositoryImpl.kt
@@ -5,22 +5,44 @@ import ee.pw.edu.pl.data.datasource.images.ImageRemoteDataSource
 import ee.pw.edu.pl.domain.core.result.UseCaseResult
 import ee.pw.edu.pl.domain.repository.ProfileRepository
 import ee.pw.edu.pl.domain.usecase.profile.EditProfileForm
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 class ProfileRepositoryImpl(
     private val imageRemoteDataSource: ImageRemoteDataSource,
 ) : ProfileRepository {
-    override suspend fun updateProfile(profileForm: EditProfileForm): UseCaseResult<Unit> {
-        val profileUrl = imageRemoteDataSource.uploadImage(profileForm.profileImage).first()
+    override suspend fun updateProfile(profileForm: EditProfileForm): UseCaseResult<Unit> =
+        coroutineScope {
+            Log.d("ProfileRepository", "starting upload")
+            val profileChannel = Channel<String?>()
+            launch {
+                val profileUrl = imageRemoteDataSource.uploadImage(profileForm.profileImage).first()
+                profileChannel.send(profileUrl)
+            }
 
-        val imagesUrl = profileForm.images.map { image ->
-            imageRemoteDataSource.uploadImage(image).first()
-        }
-        if (profileUrl == null || imagesUrl.any { it == null }) {
+            val imagesChannel = Channel<String?>()
+            for (image in profileForm.images) {
+                launch {
+                    val imageUrl = imageRemoteDataSource.uploadImage(image).first()
+                    imagesChannel.send(imageUrl)
+                }
+            }
+            val profileUrl = profileChannel.receive()
+            Log.d("ProfileRepository", "profile $profileUrl")
+
+            var allImages = emptyList<String?>()
+            repeat(profileForm.images.size) {
+                val imageUrl = imagesChannel.receive()
+                Log.d("ProfileRepository", "received $imageUrl")
+                allImages = allImages + imageUrl
+            }
+            if (profileUrl == null || allImages.any { it == null }) {
 //            TODO handle error
-            Log.e("ProfileRepository", "profile or images are null $profileUrl $imagesUrl")
-        }
+                Log.e("ProfileRepository", "profile or images are null $profileUrl $allImages")
+            }
 //        TODO send the urls with the data to the server
-        return UseCaseResult.Ok(Unit)
-    }
+            UseCaseResult.Ok(Unit)
+        }
 }
