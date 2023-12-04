cfb1e280167826efa447213668ec9da6e9559475
Michal Ankiersztajn <michalankiersztajn@gmail.com>
2023-12-04 09:33:13 +0100

    Setup SetTestUseCase with SetTestForm and test models

---
+++ domain/src/main/java/ee/pw/edu/pl/domain/usecase/account/SetTestForm.kt
@@ -0,0 +1,21 @@
+package ee.pw.edu.pl.domain.usecase.account
+
+data class SetTestForm(
+    val weight: Int,
+    val height: Int,
+    val eyes: TestAnswer,
+    val hair: TestAnswer,
+    val tattoo: TestAnswer,
+    val sport: TestAnswer,
+    val education: TestAnswer,
+    val recreation: TestAnswer,
+    val family: TestAnswer,
+    val charity: TestAnswer,
+    val people: TestAnswer,
+    val wedding: TestAnswer,
+    val belief: TestAnswer,
+    val money: TestAnswer,
+    val religious: TestAnswer,
+    val mind: TestAnswer,
+    val humour: TestAnswer,
+)
---
+++ domain/src/main/java/ee/pw/edu/pl/domain/usecase/account/SetTestUseCase.kt
@@ -0,0 +1,7 @@
+package ee.pw.edu.pl.domain.usecase.account
+
+class SetTestUseCase {
+    suspend operator fun invoke(form: SetTestForm) {
+//        TODO implement
+    }
+}
