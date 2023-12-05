d3169a16cd4e01c5b1a5e414b491fb4cd2541335
Michal Ankiersztajn <aandrobrain@gmail.com>
2023-12-05 13:57:51 +0100

    Setup additional ApiResponse function connected to status codes

--- data/src/main/java/ee/pw/edu/pl/data/model/ApiResponse.kt
+++ data/src/main/java/ee/pw/edu/pl/data/model/ApiResponse.kt
@@ -10,21 +10,27 @@ sealed class ApiResponse<out T> {
     ) : ApiResponse<T>()
 
     data class Error(
-        val exception: ApiException
+        val exception: ApiException,
     ) : ApiResponse<Nothing>() {
-        override fun isNotFound() = exception.httpCode == 404
         override fun isUnauthorized() = exception.httpCode == 401
+        override fun isForbidden() = exception.httpCode == 403
+        override fun isNotFound() = exception.httpCode == 404
+        override fun isTooManyRequests() = exception.httpCode == 429
+        override fun isServerError() = exception.httpCode >= 500
     }
 
     data class NetworkError(
-        val throwable: Throwable
+        val throwable: Throwable,
     ) : ApiResponse<Nothing>()
 
     open fun isNotFound() = false
     open fun isUnauthorized() = false
+    open fun isTooManyRequests() = false
+    open fun isForbidden() = false
+    open fun isServerError() = false
 }
 
 class ApiException(
     val httpCode: Int,
-    val errorBody: Response<*>?
+    val errorBody: Response<*>?,
 ) : Exception()
